0f76ec860f32206d80ac866e6579305c8fc67696
Christian Oliveira <christianoliveira1908@hotmail.com>
2023-05-23 08:53:36 -0300

    Implemented user register.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,9 @@
 using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UserManager.Data.DTO;
+using UserManager.Models;
 
 namespace UserManager.Controller;
 
@@ -8,8 +11,26 @@ namespace UserManager.Controller;
 [Route("[Controller]")]
 public class UserController : ControllerBase
 {
+    private IMapper _mapper;
+    private UserManager<User> _userManager;
+
+    public UserController(IMapper mapper, UserManager<User> userManager)
+    {
+        this._mapper = mapper;
+        this._userManager = userManager;
+    }
+
     [HttpPost]
-    public IActionResult Register(CreateUserDTO userDTO){
-        throw new NotImplementedException();
+    public async Task<IActionResult> Register(CreateUserDTO userDTO){
+        User user = _mapper.Map<User>(userDTO);
+
+        /* Using await, we don't have to set the var type to Task<T>; We can set the var with type T  */
+        IdentityResult result = await _userManager.CreateAsync(user, userDTO.Password);
+
+        if(result.Succeeded){
+            return Ok("Usuário cadastrado com sucesso");
+        }
+
+        return BadRequest(result.Errors);
     }
 }
